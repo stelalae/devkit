b4bd8e3658bd21bc27fc1b7d977f12dfe65d1679
Morlay <morlay.null@gmail.com>
2020-01-17 15:33:42 +0800

    fix(webpack-preset-assets): glslify

--- @querycap/webpack-preset-assets/src/index.ts
+++ @querycap/webpack-preset-assets/src/index.ts
@@ -28,7 +28,7 @@ export const withAssetsPreset = () => (c: Configuration, _2: TState) => {
     {
       // xxx.glsl
       test: /\.(glsl|frag|vert)$/,
-      use: [glslLoader],
+      use: [require.resolve("transform-loader") + "?glslify"],
     },
     {
       // xxx.svg
@@ -53,17 +53,3 @@ export const withAssetsPreset = () => (c: Configuration, _2: TState) => {
     },
   );
 };
-
-function glslLoader(content: string) {
-  const code = content
-    .trim() // strip whitespace at the start/end
-    .replace(/\s*\/\/[^\n]*\n/g, "\n") // strip double-slash comments
-    .replace(/\n+/g, "\n") // collapse multi line breaks
-    .replace(/\n\s+/g, "\n") // strip identation
-    .replace(/\s?([+-/*=,])\s?/g, "$1") // strip whitespace around operators
-    .replace(/([;(),{}])\n(?=[^#])/g, "$1"); // strip more line breaks
-
-  return `export default ${JSON.stringify(code)};`;
-}
-
-glslLoader.seperable = true;
